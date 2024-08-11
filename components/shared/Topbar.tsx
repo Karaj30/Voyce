3f44e745f1768126cd1e45b363ff7fdf6f8a1fc4
dhiru <sachimitra3668@gmail.com>
2024-08-11 22:01:22 +0530

    new changes

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,48 +1,43 @@
 import { OrganizationSwitcher, SignedIn, SignOutButton } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 import Image from "next/image";
 import Link from "next/link";
-import {dark} from '@clerk/themes'
 
 function Topbar() {
-const isUserLoggedIn = true;
+  return (
+    <nav className='topbar'>
+      <Link href='/' className='flex items-center gap-4'>
+        <Image src='/logo.svg' alt='logo' width={28} height={28} />
+        <p className='text-heading3-bold text-light-1 max-xs:hidden'>Voyce</p>
+      </Link>
 
-    return (
-    <nav className="topbar">
-       <Link href="/" className="flex items-center gap-4">
-        <Image src="/assets/logo.svg" 
-        alt="logo"
-        width={28} 
-        height={28} 
-        unoptimized={true} />
-        <p className="text-heading3-bold text-light-1 max-xs:hidden">Voyce</p>
-       </Link>
-
-       <div className="flex items-center gap-1">
-         <div className="block md:hidden">
-           <SignedIn>
+      <div className='flex items-center gap-1'>
+        <div className='block md:hidden'>
+          <SignedIn>
             <SignOutButton>
-                <div className="flex cursor-pointer">
-                    <Image
-                      src="/assets/logout.svg"
-                      alt="logout"
-                      width={24}
-                      height={24}
-                    />
-                </div>
+              <div className='flex cursor-pointer'>
+                <Image
+                  src='/assets/logout.svg'
+                  alt='logout'
+                  width={24}
+                  height={24}
+                />
+              </div>
             </SignOutButton>
-           </SignedIn>
-         </div>
-         <OrganizationSwitcher
-           appearance={{
+          </SignedIn>
+        </div>
+
+        <OrganizationSwitcher
+          appearance={{
             baseTheme: dark,
             elements: {
-                organizationSwitcherTrigger: "py-2 px-4"
-            }
-           }}
+              organizationSwitcherTrigger: "py-2 px-4",
+            },
+          }}
         />
-       </div>
+      </div>
     </nav>
-    )
+  );
 }
 
 export default Topbar;
